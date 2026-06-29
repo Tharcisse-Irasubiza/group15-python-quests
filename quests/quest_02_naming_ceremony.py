d529e3306d935671dfe8f0e5b56d82f614576600
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-06-29 02:04:42 -0700

    Variables|naming ceremony

---
+++ quests/quest_02_naming_ceremony.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Create variables
+hero_name = "Captain Thunder"
+superpower = "controlling lightning"
+
+# Print a sentence introducing the hero
+print(f"{hero_name} has the superpower of {superpower}!")

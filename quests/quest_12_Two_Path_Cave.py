7f2178025704a0adbe0652ccb08f42005766d18c
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-06-29 02:13:55 -0700

    if else-Two_Path_Cave

---
+++ quests/quest_12_Two_Path_Cave.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Correct password
+correct_password = "Python123"
+
+# Ask the user to enter a password
+password = input("Enter the password: ")
+
+# Check the password
+if password == correct_password:
+    print("Access Granted")
+else:
+    print("Access Denied")

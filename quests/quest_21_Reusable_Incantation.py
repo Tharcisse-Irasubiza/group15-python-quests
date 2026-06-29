549c09f86db169c2084b8e7de560c95e5a4ab2ff
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-06-29 02:19:40 -0700

    Function-Reusable Incantation

---
+++ quests/quest_21_Reusable_Incantation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Define the function
+def greet_adventurer():
+    print("Welcome, brave adventurer!")
+
+# Call the function three times
+greet_adventurer()
+greet_adventurer()
+greet_adventurer()
